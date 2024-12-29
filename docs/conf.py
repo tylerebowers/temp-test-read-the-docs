1942eb8d69da4e92f5b8c9b5865a701228bf7672
Tyler Bowers <tylerebowers@gmail.com>
2024-12-28 20:20:35 -0500

    asd

--- docs/conf.py
+++ docs/conf.py
@@ -5,6 +5,8 @@ copyright = '2024, Tyler Bowers'
 author = 'Tyler Bowers'
 release = '2.4.4'
 
+master_doc = 'index'
+
 #templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
